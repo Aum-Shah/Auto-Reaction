f2dee7f9331ac5aa8d31c6b5631593fd3ab41b62
Aum Shah <101273167+Aum-Shah@users.noreply.github.com>
2024-08-01 04:21:43 +0530

    Update setup-config.js

--- setup-config.js
+++ setup-config.js
@@ -17,52 +17,6 @@ function updatePackageName(packageName) {
   console.log(`Package name set to ${packageName}`);
 }
 
-// Function to generate deploy.yml
-function generateDeployYml(packageName) {
-  const deployYmlContent = `
-  name: ${packageName}
-
-  on: 
-    workflow_dispatch:
-
-  jobs:
-    deploy:
-      runs-on: ubuntu-latest
-      name: Deploy
-      steps:
-        - uses: actions/checkout@v4
-        - name: Set up Node.js
-          uses: actions/setup-node@v3
-          with:
-            node-version: 'latest'
-        - name: Install dependencies
-          run: npm install
-        - name: Run setup-config script
-          run: node setup-config.js
-        - name: Deploy
-          uses: cloudflare/wrangler-action@v3
-          with:
-            apiToken: ${{ secrets.CLOUDFLARE_API_TOKEN }}
-            accountId: ${{ secrets.CLOUDFLARE_ACCOUNT_ID }}
-            secrets: |
-              BOT_TOKEN
-              BOT_USERNAME
-              EMOJI_LIST
-              RESTRICTED_CHATS
-          env:
-            BOT_TOKEN: ${{ secrets.BOT_TOKEN }}
-            BOT_USERNAME: ${{ secrets.BOT_USERNAME }}
-            EMOJI_LIST: ${{ secrets.EMOJI_LIST }}
-            RESTRICTED_CHATS: ${{ secrets.RESTRICTED_CHATS }}
-  `;
-  const workflowsDir = '.github/workflows';
-  if (!fs.existsSync(workflowsDir)) {
-    fs.mkdirSync(workflowsDir, { recursive: true });
-  }
-  fs.writeFileSync(path.join(workflowsDir, 'deploy.yml'), deployYmlContent);
-  console.log('Generated deploy.yml with dynamic name.');
-}
-
 // Main execution
 const packageName = process.env.PACKAGE_NAME;
 
@@ -73,4 +27,3 @@ if (!packageName) {
 
 generateWranglerToml(packageName);
 updatePackageName(packageName);
-generateDeployYml(packageName);
